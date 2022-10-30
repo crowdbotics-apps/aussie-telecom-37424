7f751f778f8bb2343e5b24ec50706f59fcc50054
Crowdbotics <team@crowdbotics.com>
2022-10-30 09:49:19 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "aussie_telecom_37424",
+  url: "https://aussie_telecom_37424.botics.co",
+  api: "https://aussie_telecom_37424.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
